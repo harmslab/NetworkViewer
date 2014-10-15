2722182cc4e4008a3a1d39c347a5868417e96072
Zachary Sailer <zachsailer@gmail.com>
2014-10-15 16:58:53 -0700

    Add text to nodes

--- NetworkViewer/js/network.js
+++ NetworkViewer/js/network.js
@@ -10,8 +10,8 @@ var Network = function (svg, system) {
     this.width = this.svg.attr("width");
     this.height = this.svg.attr("height"); 
     this.charge = -100;
-    this.link_distance = 100
-    this.color_on = true;
+    this.link_distance = 60
+    this.color_on = false;
     this.system = system;
     
     // Create an svg canvas for D3 plot
@@ -45,21 +45,18 @@ Network.prototype.build_network = function() {
         .data(system.links)
         .enter().append("line")
         .attr("class", "graph_link");
-   
         
     var graph_node = this.svg.selectAll(".graph_node")
         .data(system.nodes)
         .enter().append("circle")
         .attr("class", "graph_node")
         .attr("id", function(d) {return d.index})
-        .attr("r", 10)
+        .attr("r", 13)
         .attr("cx", function(d) {return d.x;})
         .attr("cy", function(d) {return d.y;})
-        .call(graph_force.drag);    
-    
+        .call(graph_force.drag); 
     
     graph_force.on("tick", function () {
-        
         graph_node   
             .attr("cx", function(d) { return d.x; })
             .attr("cy", function(d) { return d.y; });
@@ -69,13 +66,15 @@ Network.prototype.build_network = function() {
             .attr("y1", function(d) { return d.source.y; })
             .attr("x2", function(d) { return d.target.x; })
             .attr("y2", function(d) { return d.target.y; });
-        
     });
+    
     
     this.graph_force = graph_force;
     this.graph_link = graph_link;
     this.graph_node = graph_node;
     this.system = system;
+    this.add_node_names();
+    
 };
 
 
@@ -86,3 +85,49 @@ Network.prototype.color_network = function() {
     this.graph_node
         .attr("fill", function(d){ return colors(d.value*70)});
 };
+
+
+Network.prototype.add_node_names = function() {
+    // Add names to each node if given.
+    var system = this.system;
+    var graph_force = this.graph_force;
+    var graph_node = this.graph_node;
+    var graph_link = this.graph_link;
+    
+    function x_pos(name){
+        var l = name.length;
+        var pos = -l/2;
+        return 8*pos;
+    }
+    
+    var graph_text = this.svg.selectAll("text")
+        .data(system.nodes)
+        .enter().append("text")
+        .attr("dx", function(d){return d.x + x_pos(d.name);})
+        .attr("dy", function(d) {return d.y + 5;})
+        .text(function(d) { 
+            return d.name;
+        })
+        .call(graph_force.drag);   
+    
+    graph_force.on("tick", function () {    
+        graph_text
+            .attr("dx", function(d){return d.x + x_pos(d.name);})
+            .attr("dy", function(d) {return d.y + 5;})
+            
+        graph_node   
+            .attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; });
+        
+        graph_link 
+            .attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
+    });
+        
+    this.graph_text = graph_text;
+    this.graph_force = graph_force;
+    this.graph_node = graph_node;
+    this.graph_link = graph_link;
+}
